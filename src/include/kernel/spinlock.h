19f2afafbf1e3c0962bea66de706a64deaab9bda
Ryan1202 <ryan1202@foxmail.com>
2025-05-13 23:40:40 +0800

    spinlock: 新增 通过DEBUG宏实现可选的支持保存对spinlock加锁的线程信息

--- src/include/kernel/spinlock.h
+++ src/include/kernel/spinlock.h
@@ -3,7 +3,9 @@
 
 #include "kernel/driver_interface.h"
 extern struct task_s *current_task;
-typedef volatile int  spinlock_t;
+
+#ifndef DEBUG
+typedef volatile int spinlock_t;
 
 #define SPINLOCK(lock) spinlock_t lock = 0;
 
@@ -14,21 +16,50 @@ static inline void spinlock_init(spinlock_t *lock) {
 	lock = 0;
 }
 
+#define SPINLOCK_GET(lock) *lock
+#else
+typedef volatile struct {
+	int			   lock;
+	struct task_s *owner;
+} spinlock_t;
+
+#define SPINLOCK(lock) spinlock_t lock = {0, NULL};
+
+#define SPINLOCK_INIT(lock) \
+	{ (lock) = {0, NULL}; }
+
+#define SPINLOCK_GET(lock) lock->lock
+static inline void spinlock_init(spinlock_t *lock) {
+	lock->lock	= 0;
+	lock->owner = NULL;
+}
+#endif
+
 static inline void spin_lock(spinlock_t *lock) {
 	// 使用gcc提供的__sync_bool_compare_and_swap()实现原子操作
-	while (!__sync_bool_compare_and_swap(lock, 0, 1)) {
-		while (*lock)
+	while (!__sync_bool_compare_and_swap(&SPINLOCK_GET(lock), 0, 1)) {
+		while (SPINLOCK_GET(lock))
 			;
 	}
+#ifdef DEBUG
+	lock->owner = current_task;
+#endif
 }
 
 static inline int spin_try_lock(spinlock_t *lock) {
-	return __sync_bool_compare_and_swap(lock, 0, 1);
+	int ret = __sync_bool_compare_and_swap(&SPINLOCK_GET(lock), 0, 1);
+#ifdef DEBUG
+	if (ret) lock->owner = current_task;
+#endif
+	return ret;
 }
 
 static inline void spin_unlock(volatile spinlock_t *lock) {
 	__asm__ __volatile__("" ::: "memory");
-	*lock = 0;
+	SPINLOCK_GET(lock) = 0;
+#ifdef DEBUG
+	lock->owner = NULL;
+#endif
 }
 
 // 获取自旋锁的同时禁用中断并保存中断状态
